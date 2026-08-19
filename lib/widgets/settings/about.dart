b3808e22de9a7606ca20c74a42689f02d4d3e8e1
Bilal Suleyman <workubilal@gmail.com>
2026-08-19 04:35:30 +0300

    show full changelog

--- lib/widgets/settings/about.dart
+++ lib/widgets/settings/about.dart
@@ -9,7 +9,7 @@ import 'package:rzr/utils/env_var.dart';
 import 'package:rzr/utils/toast/common.dart';
 // import 'package:rzr/utils/toast/common.dart';
 import 'package:rzr/widgets/settings/link_icon.dart';
-import 'package:rzr/screens/changelog_screen.dart';
+import 'package:rzr/widgets/markdown/styled_markdown.dart';
 import 'package:rzr/widgets/settings/show_donate_dialog.dart';
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -137,15 +137,41 @@ Future<void> openAboutDialog(BuildContext context) async {
                 ),
                 ListTile(
                   title: Text(L10n.of(context).appChangelog),
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => ChangelogScreen(
-                        lastVersion: Prefs().lastAppVersion ?? version,
-                        currentVersion: version,
-                        onComplete: () => Navigator.of(context).pop(),
+                  onTap: () async {
+                    var content =
+                        await rootBundle.loadString('assets/CHANGELOG.md');
+                    // Remove the first line (file title) because dialog shows its own title
+                    final lines = content.split('\n');
+                    if (lines.isNotEmpty) {
+                      // remove first line regardless of whether it's empty or not
+                      lines.removeAt(0);
+                      content = lines.join('\n');
+                    }
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext ctx) => AlertDialog(
+                        title: Text(L10n.of(ctx).appChangelog),
+                        content: ConstrainedBox(
+                          constraints: const BoxConstraints(
+                            maxWidth: 700,
+                            minWidth: 300,
+                          ),
+                          child: SingleChildScrollView(
+                            child: StyledMarkdown(data: content),
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.of(ctx).pop(),
+                            child: Text(
+                              L10n.of(ctx).commonOk,
+                              style: TextStyle(color: Prefs().accentColor),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
                 ListTile(
                   title: Text(L10n.of(context).appDonate),
